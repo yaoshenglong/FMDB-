58a75d32bbb7d25677cb1bba07d558218aba2ede
姚胜龙 <shenglong.yao@yi23.net>
2017-02-08 17:47:43 +0800

    添加查询和删除方法

--- FMDB测试/FMDB测试/Controller/ViewController.m
+++ FMDB测试/FMDB测试/Controller/ViewController.m
@@ -25,83 +25,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"FMDB联系";
+    self.title = @"FMDB练习";
 }
 
-//insert data 插入数据
-- (void)insertStudents {
+#pragma mark - Button Action
 
-    for (int i = 0; i < 20; i ++) {
-        Person *student = (Person *)_students[i];
-//        NSString *name = [NSString stringWithFormat:@"张_%d", i];
-//        NSInteger age = arc4random_uniform(40);
-//        [self.studentDb executeUpdate:@"INSERT INTO t_student (name, age) VALUES(?,?);", name, @(age)];
-        BOOL result = [self.studentDb executeUpdate:@"INSERT INTO t_student (name, age) VALUES(?,?);", student.name, @(student.age)];
-        if (result) {
-            NSLog(@"插入学生成功");
-        }
-        else {
-             NSLog(@"插入学生失败");
-        }
-//        [self.studentDb executeUpdate:@"INSERT INTO t_student (name, age) VALUES(%@, %ld);", student.name, student.age];
-    }
-}
-
-- (void)insertAPersonWithPersonId:(NSNumber *)personId {
-    
-}
-
-// delete data 删除数据
-- (void)deleteStudents {
-
-    BOOL result = [self.studentDb executeUpdate:@"DELETE FROM t_student;"];
-//    BOOL result = [self.studentDb executeUpdate:@"DROP TABLE IF EXISTS t_student;"];
-    if (result) {
-        NSLog(@"删除表数据成功");
-    }
-    else {
-        NSLog(@"删除失败, 创建一个学生表");
-        [self.studentDb executeUpdate:@"CREATE TABLE IF NOT EXISTS t_student (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL, age integer NOT NULL);"];
-    }
-}
-
-//query data 查询数据
-- (void)queryStudents {
-    FMResultSet *resultSet = [self.studentDb executeQuery:@"SELECT * FROM t_student"];
-    //遍历结果
-    while ([resultSet next]) {
-        NSInteger studentId = [resultSet intForColumn:@"id"];
-        NSString *name = [resultSet stringForColumn:@"name"];
-        NSInteger age = [resultSet intForColumn:@"age"];
-
-        NSLog(@"\n studentId:%ld  name:%@  age:%ld \n", studentId, name, age);
-    }
-}
-
-- (void)createStudents {
-
-    _students = [NSMutableArray array];
-    for (int i = 0; i < 20 ; i ++) {
-        Person *person = [[Person alloc] init];
-        person.name = [NSString stringWithFormat:@"张%d", i+1];
-        person.age = 18 + i;
-        person.sex = (i % 2 == 0) ? @"男" : @"女";
-        person.personId = @(i+100);
-
-        [_students addObject:person];
-    }
-}
-- (IBAction)insert:(id)sender {
-    [self insertStudents];
-}
-- (IBAction)query:(id)sender {
-    [self queryStudents];
-}
-- (IBAction)delete:(id)sender {
-    [self deleteStudents];
-}
-
-#pragma mark - 学生列表测试
 //添加学生
 - (IBAction)addAllStudents:(id)sender {
     
@@ -114,6 +42,32 @@
         [[StudentManager shareManager] insertStudent:student];
     }
 }
+
+//插入冲突的数据
+- (IBAction)conflictInsert:(id)sender {
+
+}
+
+//查询某个学生
+- (IBAction)queryOne:(id)sender {
+
+}
+
+//查询所有的学生
+- (IBAction)queryAll:(id)sender {
+
+}
+
+//删除一个
+- (IBAction)deleteOne:(id)sender {
+
+}
+
+//删除所有的
+- (IBAction)deleteAll:(id)sender {
+
+}
+
 
 - (IBAction)showStudentList:(UIButton *)sender {
     StudentListController *stuListVC = [[StudentListController alloc] initWithNibName:@"StudentListController" bundle:nil];
